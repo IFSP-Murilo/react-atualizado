66c4b8a28725677f62a0bc3288b35bfa52d86cac
Murilo <muriloflimaa@gmail.com>
2023-03-20 22:25:45 -0300

    Alteracao e exclusao

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
-
 import './App.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Rotas from './rotas/Rotas';
-import Dashboard from './pages/Dashboard';
-import ProfessorLista from './pages/professor/Lista';
-import ProfessorIncluir from './pages/professor/Incluir';
-import ProfessorAlterar from './pages/professor/Alterar';
-import CidadeLista from './pages/cidade/Lista';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CidadeIncluir from './pages/cidade/Incluir';
-
+import CidadeLista from './pages/cidade/Lista';
+import Dashboard from './pages/Dashboard';
+import ProfessorAlterar from './pages/professor/Alterar';
+import ExcluirProfessor from './pages/professor/Excluir';
+import ProfessorIncluir from './pages/professor/Incluir';
+import ProfessorLista from './pages/professor/Lista';
+import Rotas from './rotas/Rotas';
 
 function App() {
   return (
-     <>
-     <BrowserRouter>
-      <Routes> 
-        <>  
-        <Route element={<Rotas/>}>
-          <Route path="/" element={<Dashboard/>} /> 
-          <Route path="/home" element={<Dashboard/>} /> 
-          <Route path="/professor/lista" element={<ProfessorLista/>}/>
-          <Route path="/professor/incluir" element={<ProfessorIncluir/>}/>
-          <Route path="/professor/alterar/:id" element={<ProfessorAlterar/>}/>
-          <Route path="/cidade/lista" element={<CidadeLista/>}/>
-          <Route path="/cidade/incluir" element={<CidadeIncluir/>}/>
-        </Route>        
-        </>
-      </Routes>  
-     </BrowserRouter>
-     </>
-     
+    <>
+      <BrowserRouter>
+        <Routes>
+          <>
+            <Route element={<Rotas />}>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/home" element={<Dashboard />} />
+              <Route path="/professor/lista" element={<ProfessorLista />} />
+              <Route path="/professor/incluir" element={<ProfessorIncluir />} />
+              <Route path="/professor/alterar/:id" element={<ProfessorAlterar />} />
+              <Route path="/professor/excluir/:id" element={<ExcluirProfessor />} />
+              <Route path="/cidade/lista" element={<CidadeLista />} />
+              <Route path="/cidade/incluir" element={<CidadeIncluir />} />
+            </Route>
+          </>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,46 +1,40 @@
-
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import { USER_PHOTO } from '../../config/Config';
 
 const Header = ({ isToogle }) => {
-  
-    const [toggled, setToggled] = useState(false);   
+  const [toggled, setToggled] = useState(false);
 
-    const toogleClik = () => {
+  const toogleClik = () => {
+    setToggled(!toggled);
+    isToogle(toggled);
+  };
 
-         setToggled(!toggled);
-         isToogle(toggled)  
-    }  
-
-
-
-    return (
-      <Fragment> 
-        <header className="app-header">
+  return (
+    <Fragment>
+      <header className="app-header">
         <div className="app-leftarea">
-            <h3>SISTEMA<span>IFSP</span></h3>
+          <h3>
+            SISTEMA<span>IFSP</span>
+          </h3>
         </div>
         <div className="app-toggle">
-            <i id="sidebar_toggle"><FaIcons.FaBars/></i>
+          <i id="sidebar_toggle">
+            <FaIcons.FaBars />
+          </i>
         </div>
         <div className="profile">
-            <img src={USER_PHOTO} alt="foto"/>
-            <span>Francisco Santos</span>
-            <div className="logout">
-                <i><FaIcons.FaSignOutAlt/></i> 
-            </div>
+          <img src={USER_PHOTO} width={40} height={40} alt="foto" className="rounded-circle" />
+          <span>Murilo Fernandes</span>
+          <div className="logout">
+            <i>
+              <FaIcons.FaSignOutAlt />
+            </i>
+          </div>
         </div>
-        
-        </header>
+      </header>
     </Fragment>
-    
-  )
-}
+  );
+};
 
-export default Header
-
-
-
-
-
+export default Header;
